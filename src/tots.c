6df3bb07df5f7d03fadd665e7e8bdb8411e526ee
Jake Kim <jake@getawair.com>
2023-09-15 11:54:44 +0900

    initial commit with API

---
+++ src/tots.c
@@ -0,0 +1,28 @@
+#include "tots.h"
+
+#include <search.h>
+
+struct tots {
+    struct hsearch_data htab;
+}
+
+tots *tots_make(size_t n)
+{
+}
+
+void *tots_hash(tots *t, char *k, void *v)
+{
+}
+
+void *tots_pick(tots *t, char *k)
+{
+}
+
+void *tots_drop(tots *t, char *k)
+{
+}
+
+void tots_trash(tots *t)
+{
+}
+
---
+++ src/tots.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef struct tots tots;
+
+tots *tots_make(size_t n);
+void *tots_hash(tots *t, char *k, void *v);
+void *tots_pick(tots *t, char *k);
+void *tots_drop(tots *t, char *k);
+void tots_trash(tots *t);
+
